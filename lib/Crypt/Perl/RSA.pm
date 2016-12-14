46adf63b817382842305a5842a6109e2bce78b67
Felipe Gasper <felipe@felipegasper.com>
2016-12-13 20:38:30 -0500

    almost ready for 0.01?

---
+++ lib/Crypt/Perl/RSA.pm
@@ -0,0 +1,81 @@
+package Crypt::Perl::RSA;
+
+use strict;
+use warnings;
+
+=encoding utf-8
+
+=head1 NAME
+
+Crypt::Perl::RSA - RSA in pure Perl (really!)
+
+=head1 SYNOPSIS
+
+    my $prkey1 = Crypt::Perl::RSA::Parse::private($pem_or_der);
+    my $pbkey1 = Crypt::Perl::RSA::Parse::public($pem_or_der);
+
+    #----------------------------------------------------------------------
+
+    my $prkey = Crypt::Perl::RSA::Generate::generate(2048);
+
+    my $der = $prkey->to_der();
+    my $der2 = $prkey->to_pem();
+
+    #----------------------------------------------------------------------
+
+    my $msg = 'My message';
+
+    my $sig = $prkey->sign_RS256($msg);
+
+    die 'Wut' if !$prkey->verify_RS256($msg, $sig);
+
+    die 'Wut' if !$prkey->verify_RS256($msg, $sig);
+
+=head1 DISCUSSION
+
+See the documentation for C<Crypt::Perl::RSA::PublicKey> and
+C<Crypt::Perl::RSA::PrivateKey> for discussions of what these interfaces
+can do.
+
+NOTE: The ECDSA logic here is ported from Kenji Urushima’s L<jsrsasign|http://kjur.github.io/jsrsasign/>.
+
+=head1 TODO
+
+=over 4
+
+=item * I
+
+=back
+
+=head1 SECURITY
+
+RSA is safe as long as factorization is “hard”. As computers get faster, RSA
+keys have needed to get bigger and bigger to maintain the “difficulty” of
+factoring the key’s modulus. RSA will eventually no longer be viable, though:
+as RSA keys get bigger, the
+security advantage of increasing their size diminishes.
+
+=head1 SPEED
+
+Key generation is pretty slow—potentially unacceptably so for general use.
+It can be made faster by having an XS-based backend available for
+L<Math::BigInt> and L<Bytes::Random::Secure::Tiny>, but it’ll still be pretty
+pokey. The difficulty rests in prime number generation, which is handled by
+L<Math::ProvablePrime>.
+
+=head1 TODO
+
+This minimal set of functionality can be augmented as feature requests come in.
+Ideas:
+
+=over 4
+
+=item * Support signature schemes besides PKCS #1 v1.5
+
+=item * Use faster prime-number-finder logic if it’s available.
+
+=back
+
+=cut
+
+1;
