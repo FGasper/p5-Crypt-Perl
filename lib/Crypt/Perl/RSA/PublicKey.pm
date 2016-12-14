cf290986f8a55547b098dbbf8bc8f9fde98251d0
Felipe Gasper <felipe@felipegasper.com>
2016-12-13 20:45:24 -0500

    ensure that the public exponent is a BigInt

--- lib/Crypt/Perl/RSA/PublicKey.pm
+++ lib/Crypt/Perl/RSA/PublicKey.pm
@@ -1,15 +1,30 @@
 package Crypt::Perl::RSA::PublicKey;
 
+use strict;
+use warnings;
+
 use parent qw(
     Class::Accessor::Fast
     Crypt::Perl::RSA::KeyBase
 );
+
+use Crypt::Perl::BigInt ();
 
 BEGIN {
     __PACKAGE__->mk_ro_accessors('publicExponent');
 
     *exponent = \&publicExponent;
     *E = \&exponent;
+}
+
+sub new {
+    my ($class, @args) = @_;
+
+    my $self = $class->SUPER::new(@args);
+
+    $self->{'publicExponent'} = Crypt::Perl::BigInt->new( $self->{'publicExponent'} );
+
+    return $self;
 }
 
 sub to_der {
